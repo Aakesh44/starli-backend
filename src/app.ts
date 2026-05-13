4a5d84cc9d6c9571a9be33bd24d8790346ad19be
Aakesh44 <aakeshviswanathan@gmail.com>
2026-05-13 18:32:28 +0530

    final push

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,13 @@ const app = express();
 app.disable("x-powered-by");
 
 app.use(helmet());
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({
+    origin: [
+        "http://localhost:3005",
+        "https://starli-io.vercel.app"
+    ],
+    credentials: true
+}));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(morgan("dev"));
